28578e004b72f26485f486f2ff60b5e6251ff044
Minhkun912 <minhnguyenduc506@gmail.com>
2023-03-20 19:30:21 +0700

    first commit

---
+++ src/bai1/MyListTest.java
@@ -0,0 +1,12 @@
+package bai1;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> listInteger = new MyList<Integer>();
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+        listInteger.add(4);
+        System.out.println("Element get"+listInteger.get(2));;
+    }
+}
